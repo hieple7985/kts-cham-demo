e43d879b5089f87fa721cab89dc4f937670c0a42
hieple7985 <hieple7985@users.noreply.github.com>
2026-01-21 11:30:00 +0700

    feat: add app assets configuration

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String _imagePath = 'images';
+
+  static const String cucaWriting = '$_imagePath/cuca_writing.png';
+  static const String cucaHeadset = '$_imagePath/cuca_headset.png';
+  static const String cucaIdea = '$_imagePath/cuca_idea.png';
+  static const String cucaThumbsUp = '$_imagePath/cuca_thumbs_up.png';
+}
